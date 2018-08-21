9bab629d721e7262c6a68bd701cc6e172f9c5c1c
Dylan <dylan@dev-alizon.fr>
2018-08-21 15:22:33 +0200

    Modification push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -4,6 +4,6 @@ meteor build .build/ --server-only --directory
 cd .build/bundle/
 git init
 git add .
-git commit -m "Build"
+git commit -m -q "Build"
 git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_6f6dfbaf-7f8a-4237-bc5b-5047c6412f5c.git
-git --verbose --force push cleverapps master
+git push -v cleverapps master
