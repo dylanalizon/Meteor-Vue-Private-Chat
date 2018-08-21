476306b908539bf12b932db5a4921df834ff4f09
Dylan <dylan@dev-alizon.fr>
2018-08-21 14:54:29 +0200

    Script push

---
+++ .travis/push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+meteor build .build/ --server-only --directory
+cd .build/bundle/
+git init
+git add .
+git commit -m "Build"
+git remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_6f6dfbaf-7f8a-4237-bc5b-5047c6412f5c.git
+git --verbose --force push cleverapps master
